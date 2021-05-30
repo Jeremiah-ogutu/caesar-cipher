2b34bcbd72c21dce754bc7abbb0e52e84822f9c1
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-05-30 18:16:51 +0300

    add initial commit

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -1,3 +1,13 @@
 public class Caesar {
 
+        public static void main(String[] args) {
+            int key = 7;
+            String text ="Welcome to my first java program";
+            char[] chars = text.toCharArray();
+            for(char c : chars){
+                c += key;
+                System.out.print(c);
+            }
+
+        }
 }
