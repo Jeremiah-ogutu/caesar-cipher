a197ab3dc086eb6acddc21893d04a854957d0ba5
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-05-30 23:37:21 +0300

    delition

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -1,13 +1,14 @@
-public class Caesar {
+//public class Caesar {
+//
+//        public static void main(String[] args) {
+//            int key = 7;
+//            String text ="Welcome to my first java program";
+//            char[] chars = text.toCharArray();
+//            for(char c : chars){
+//                c += key;
+//                System.out.print(c);
+//            }
+//
+//        }
+//}
 
-        public static void main(String[] args) {
-            int key = 7;
-            String text ="Welcome to my first java program";
-            char[] chars = text.toCharArray();
-            for(char c : chars){
-                c += key;
-                System.out.print(c);
-            }
-
-        }
-}
